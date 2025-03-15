2abce70a712963be8102a96b00d71fcb8d9dc519
dev-null-loop <dev.null.loop@gmail.com>
2025-03-15 12:46:38 +0200

    +private_ip output

--- opensearch_cluster/outputs.tf
+++ opensearch_cluster/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "The OCID of the cluster."
   value       = oci_opensearch_opensearch_cluster.this.id
 }
+
+output "private_ip" {
+  description = "The cluster's private IP address."
+  value       = oci_opensearch_opensearch_cluster.this.opensearch_private_ip
+}
